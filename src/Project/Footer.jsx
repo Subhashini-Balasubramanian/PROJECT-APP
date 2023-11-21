4514970766f6198c6d1b61f1f9eef0680bb5ef74
Subhashini-Balasubramanian <727722euit189@skcet.ac.in>
2023-11-21 12:52:07 +0530

    commit

--- src/Project/Footer.jsx
+++ src/Project/Footer.jsx
@@ -3,7 +3,7 @@ import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
- import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Facebook, Instagram, Twitter, YouTube} from "@mui/icons-material";
 import { Box } from "@mui/material";
 
 export default function Footer() {
@@ -35,17 +35,17 @@ export default function Footer() {
             <Typography variant="h6" color="text.primary" gutterBottom>
               Contact Us
             </Typography>
+            {/* // <Typography variant="body2" color="text.secondary">
+            //   123 Main Street, Anytown, USA
+            // </Typography> */}
             <Typography variant="body2" color="text.secondary">
-              123 Main Street, Anytown, USA
-            </Typography> 
-            <Typography variant="body2" color="text.secondary">
-              Email: relaxinn@now.com
+              Email: relaxinn@example.com
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Phone: +1 234 567 8901
+              Phone: +91 7587995430
             </Typography>
-          </Grid> 
-           <Grid item xs={12} sm={4}>
+          </Grid>
+          <Grid item xs={12} sm={4}>
             <Typography variant="h6" color="text.primary" gutterBottom>
               Follow Us
             </Typography>
@@ -59,8 +59,11 @@ export default function Footer() {
             >
               <Instagram />
             </Link>
-            <Link href="https://www.twitter.com/" color="inherit">
+            <Link href="https://www.twitter.com/" color="inherit" sx={{pr: 1}}>
               <Twitter />
+            </Link>
+            <Link href="https://youtube.com/" color="inherit">
+              <YouTube />
             </Link>
           </Grid>
         </Grid>
